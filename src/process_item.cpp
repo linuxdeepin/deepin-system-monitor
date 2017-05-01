03fea783af62f0ad169ddf86f7b0fb7641fb4ee3
Andy Stewart <lazycat.manatee@gmail.com>
2017-05-02 06:34:09 +0800

    Add interfaces.

--- src/process_item.cpp
+++ src/process_item.cpp
@@ -1,6 +1,14 @@
 #include "process_item.h"
 
-ProcessItem::ProcessItem()
+ProcessItem::ProcessItem(QString procPath)
 {
+    
+}
+
+void ProcessItem::render(int column, QRect rect, QPainter *painter) {
+    
+}
+
+void ProcessItem::renderSelection(QRect rect, QPainter *painter) {
     
 }
--- src/process_item.h
+++ src/process_item.h
@@ -8,7 +8,19 @@ class ProcessItem : public ListItem
     Q_OBJECT
     
 public:
-    ProcessItem();
+    ProcessItem(QString procPath);
+    
+    void render(int column, QRect rect, QPainter *painter);
+    void renderSelection(QRect rect, QPainter *painter);
+    
+private:
+    QString processIconPath;
+    QString processName;
+    int processCPU;
+    int processMemory;
+    int processUploadBandwidth;
+    int processDownloadBandwidth;
+    int processPid;
 };
 
 #endif
