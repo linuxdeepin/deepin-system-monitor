bbf795abe45d8696f7e0fae5e1cd0564884ebe3e
Andy Stewart <lazycat.manatee@gmail.com>
2017-05-10 02:12:19 +0800

    Remove draw background code, ready for new feature.

--- src/status_monitor.cpp
+++ src/status_monitor.cpp
@@ -9,8 +9,4 @@ StatusMonitor::StatusMonitor(QWidget *parent) : QWidget(parent)
 void StatusMonitor::paintEvent(QPaintEvent *) {
     QPainter painter(this);
     painter.setRenderHint(QPainter::Antialiasing, true);
-
-    QPainterPath path;
-    path.addRect(QRectF(rect()));
-    painter.fillPath(path, QColor("#FFA0A0"));
 }
