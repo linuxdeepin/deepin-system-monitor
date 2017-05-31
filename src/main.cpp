3242649ea5e325a08093c8dcb7b3631e9c4f4ab6
Andy Stewart <lazycat.manatee@gmail.com>
2017-05-31 13:25:06 +0800

    Add about page.

--- src/main.cpp
+++ src/main.cpp
@@ -15,6 +15,14 @@ DWIDGET_USE_NAMESPACE
 int main(int argc, char *argv[]) 
 {
     DApplication::loadDXcbPlugin();
+    
+    const char *descriptionText = QT_TRANSLATE_NOOP("MainWindow", 
+                                                    "深度系统监视器是一款设计直观易用、功能强大的系统监视器。"
+                                                    "它支持进程的CPU、内存、网络和磁盘等多种状态显示，"
+                                                    "并简化用户进程的显示和说明，方便用户更快速的使用。"
+                                                    );
+
+    const QString acknowledgementLink = "https://www.deepin.org/acknowledgments/deepin-system-monitor#thanks";
 
     DApplication app(argc, argv);
 
@@ -24,6 +32,11 @@ int main(int argc, char *argv[])
         app.setOrganizationName("deepin");
         app.setApplicationName(QObject::tr("Deepin System Monitor"));
         app.setApplicationVersion("1.0");
+        
+        app.setProductIcon(QPixmap::fromImage(QImage(Utils::getQrcPath("logo_96.svg"))));
+        app.setProductName(DApplication::translate("MainWindow", "Deepin System Monitor"));
+        app.setApplicationDescription(DApplication::translate("MainWindow", descriptionText) + "\n");
+        app.setApplicationAcknowledgementPage(acknowledgementLink);
         
         app.setTheme("dark");
         app.setWindowIcon(QIcon(Utils::getQrcPath("deepin-system-monitor.svg")));
