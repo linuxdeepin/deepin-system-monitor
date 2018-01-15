0c076e339f186131f91742c4d470aa7644ddcf5e
Andy Stewart <lazycat.manatee@gmail.com>
2018-01-15 18:01:44 +0800

    Fixed getcmdline crash error cause by cmdline.length() == 0

--- nethogs/src/inode2prog.cpp
+++ nethogs/src/inode2prog.cpp
@@ -124,24 +124,28 @@ std::string getcmdline(pid_t pid) {
 
   bool replace_null = false;
   std::string cmdline = read_file(filename);
-
-  // join parameters, keep prgname separate, don't overwrite trailing null
-  for (size_t idx = 0; idx < (cmdline.length() - 1); idx++) {
-    if (cmdline[idx] == 0x00) {
-      if (replace_null) {
-        cmdline[idx] = ' ';
+  
+  if (cmdline.length() <= 0) {
+      return "";
+  } else {
+      // join parameters, keep prgname separate, don't overwrite trailing null
+      for (size_t idx = 0; idx < (cmdline.length() - 1); idx++) {
+          if (cmdline[idx] == 0x00) {
+              if (replace_null) {
+                  cmdline[idx] = ' ';
+              }
+              replace_null = true;
+          }
       }
-      replace_null = true;
-    }
-  }
 
-  if (cmdline.length() == 0 || (cmdline[cmdline.length() - 1] != 0x00)) {
-    // invalid content of cmdline file. Add null char to allow further
-    // processing.
-    cmdline.append("\0");
-  }
+      if (cmdline.length() == 0 || (cmdline[cmdline.length() - 1] != 0x00)) {
+          // invalid content of cmdline file. Add null char to allow further
+          // processing.
+          cmdline.append("\0");
+      }
 
-  return cmdline;
+      return cmdline;
+  }
 }
 
 void setnode(unsigned long inode, pid_t pid) {
