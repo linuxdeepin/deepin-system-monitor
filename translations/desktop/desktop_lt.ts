5f04d36483e89670b2b281e30faf87156c39f47c
rekols <rekols@foxmail.com>
2018-11-23 10:46:04 +0800

    update tx config
    Change-Id: I7dd6f81b68b8e6ba6b5e6dfe99686337b546a488

---
+++ translations/desktop/desktop_lt.ts
@@ -0,0 +1,3 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="lt"><context><name>desktop</name><message><location filename="Desktop Entry]Name" line="0"/><source>Deepin System Monitor</source><translation>Deepin sistemos monitorius</translation></message><message><location filename="Desktop Entry]Comment" line="0"/><source>Monitor system process status</source><translation>Stebėti sistemos procesų būseną</translation></message><message><location filename="Desktop Entry]GenericName" line="0"/><source>System Monitor</source><translation>Sistemos monitorius</translation></message></context></TS>
