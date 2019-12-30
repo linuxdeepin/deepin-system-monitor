66653a1af1cd4a70cbd5a0b53a6b6e754e0aaa80
014021 <dingwen.zhang@archermind.com>
2019-12-30 17:30:51 +0800

    Revert "fix table row display issue with only one column"
    This reverts commit 92698699ac4eb70aaf81c475e0ec50f9d7a05560.

--- src/gui/base_item_delegate.cpp
+++ src/gui/base_item_delegate.cpp
@@ -89,52 +89,43 @@ void BaseItemDelegate::paint(QPainter *painter, const QStyleOptionViewItem &opti
     QRect textRect = rect;
 
     switch (opt.viewItemPosition) {
-    case QStyleOptionViewItem::Beginning: {
-        rect.setX(rect.x() + margin);  // left margin
+        case QStyleOptionViewItem::Beginning: {
+            rect.setX(rect.x() + margin);  // left margin
 
-        QPainterPath rectPath, roundedPath;
-        roundedPath.addRoundedRect(rect.x(), rect.y(), rect.width() * 2, rect.height(), radius,
-                                   radius);
-        rectPath.addRect(rect.x() + rect.width(), rect.y(), rect.width(), rect.height());
-        clipPath = roundedPath.subtracted(rectPath);
-        painter->setClipPath(clipPath);
-        path.addRect(rect);
-    }
-    break;
-    case QStyleOptionViewItem::Middle: {
-        path.addRect(rect);
-    }
-    break;
-    case QStyleOptionViewItem::End: {
-        rect.setWidth(rect.width() - margin);  // right margin
+            QPainterPath rectPath, roundedPath;
+            roundedPath.addRoundedRect(rect.x(), rect.y(), rect.width() * 2, rect.height(), radius,
+                                       radius);
+            rectPath.addRect(rect.x() + rect.width(), rect.y(), rect.width(), rect.height());
+            clipPath = roundedPath.subtracted(rectPath);
+            painter->setClipPath(clipPath);
+            path.addRect(rect);
+        } break;
+        case QStyleOptionViewItem::Middle: {
+            path.addRect(rect);
+        } break;
+        case QStyleOptionViewItem::End: {
+            rect.setWidth(rect.width() - margin);  // right margin
 
-        QPainterPath rectPath, roundedPath;
-        roundedPath.addRoundedRect(rect.x() - rect.width(), rect.y(), rect.width() * 2,
-                                   rect.height(), radius, radius);
-        rectPath.addRect(rect.x() - rect.width(), rect.y(), rect.width(), rect.height());
-        clipPath = roundedPath.subtracted(rectPath);
-        painter->setClipPath(clipPath);
-        path.addRect(rect);
-    }
-    break;
-    case QStyleOptionViewItem::OnlyOne: {
-        rect.setWidth(rect.width() - 2 * margin);
-        path.addRoundedRect(rect, radius, radius);
-
-    }
-    break;
-    default: {
-        painter->restore();
-        QStyledItemDelegate::paint(painter, option, index);
-        return;
-    }
+            QPainterPath rectPath, roundedPath;
+            roundedPath.addRoundedRect(rect.x() - rect.width(), rect.y(), rect.width() * 2,
+                                       rect.height(), radius, radius);
+            rectPath.addRect(rect.x() - rect.width(), rect.y(), rect.width(), rect.height());
+            clipPath = roundedPath.subtracted(rectPath);
+            painter->setClipPath(clipPath);
+            path.addRect(rect);
+        } break;
+        default: {
+            painter->restore();
+            QStyledItemDelegate::paint(painter, option, index);
+            return;
+        }
     }
 
     painter->fillPath(path, background);
 
     QString text;
     QRect iconRect;
-    if (opt.viewItemPosition == QStyleOptionViewItem::Beginning || opt.viewItemPosition == QStyleOptionViewItem::OnlyOne) {
+    if (opt.viewItemPosition == QStyleOptionViewItem::Beginning) {
         if (opt.features & QStyleOptionViewItem::HasDecoration) {
             textRect = rect;
             textRect.setX(textRect.x() + margin * 2 + kIconSize.width());
@@ -158,7 +149,7 @@ void BaseItemDelegate::paint(QPainter *painter, const QStyleOptionViewItem &opti
     }
 
     if (opt.features & QStyleOptionViewItem::HasDecoration &&
-            (opt.viewItemPosition == QStyleOptionViewItem::Beginning || opt.viewItemPosition == QStyleOptionViewItem::OnlyOne)) {
+        opt.viewItemPosition == QStyleOptionViewItem::Beginning) {
         opt.icon.paint(painter, iconRect);
     }
     painter->setPen(forground);
