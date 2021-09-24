a9392be28929e3fc58b85bfcc6fe17ab451b2f7b
yukuan <yukuan@uniontech.com>
2021-09-24 09:06:38 +0800

    fix: 切换字体后插件悬浮框显示异常
    切换字体后插件悬浮框显示异常
    
    Log: 切换字体后插件悬浮框显示异常
    
    Bug: https://pms.uniontech.com/zentao/bug-view-96408.html
    Change-Id: I6cc75d11ecb62cd320857df84726e488a1c5d3a7

--- deepin-system-monitor-plugin/gui/systemmonitortipswidget.cpp
+++ deepin-system-monitor-plugin/gui/systemmonitortipswidget.cpp
@@ -34,29 +34,9 @@ SystemMonitorTipsWidget::SystemMonitorTipsWidget(QWidget *parent)
 void SystemMonitorTipsWidget::setSystemMonitorTipsText(QStringList strList)
 {
     m_textList  = strList;
+
     //  [   CPU: 100.0%    ↓ 1000kb/s  ]
-
-    setFixedSize(fontMetrics().width(m_textList.at(0).at(0)) * 25, fontMetrics().boundingRect(m_textList.at(0)).height() * 2);
-    update();
-}
-
-
-void SystemMonitorTipsWidget::paintEvent(QPaintEvent *event)
-{
-    QFrame::paintEvent(event);
-
-    int rectWidth = rect().width();
-    int rectHeight = rect().height();
-    QPainter painter(this);
-    painter.setPen(QPen(palette().brightText(), 1));
-
-    QTextOption optionLeft;
-    optionLeft.setAlignment(Qt::AlignLeft | Qt::AlignVCenter);
-    QTextOption optionRight;
-    optionRight.setAlignment(Qt::AlignRight | Qt::AlignVCenter);
-    QTextOption optionMid;
-    optionMid.setAlignment(Qt::AlignCenter);
-
+    // 获取strList字符串，并存入cpu,mem,upLoad,download中
     QString cpu;
     QString mem;
     QString upLoad;
@@ -70,9 +50,73 @@ void SystemMonitorTipsWidget::paintEvent(QPaintEvent *event)
             downLoad = m_textList.at(i);
         else if (i == 3)
             upLoad = m_textList.at(i);
-        else { }
+        else
+            qDebug() << "do not set the text";
     }
 
+    // 设置左侧字符串宽度
+    if (cpu.length() == 3) {
+        m_leftWidth = fontMetrics().width(QString(" ") + DApplication::translate("Plugin.cpu", "CPU") + QString(": 0") + cpu + QString("%") + QString(" "));
+    } else {
+        m_leftWidth = fontMetrics().width(QString(" ") + DApplication::translate("Plugin.cpu", "CPU") + QString(": ") + cpu + QString("%") + QString(" "));
+    }
+    // 左侧宽度预留10个像素
+    m_leftWidth += 10;
+
+    // 设置右侧字符串宽度
+    if (downLoad.length() == 3) {
+        m_rightWidth = fontMetrics().width(QString("↓000") + downLoad + QString(" "));
+    } else if (downLoad.length() == 4) {
+        m_rightWidth = fontMetrics().width(QString("↓00") + downLoad + QString(" "));
+    } else if (downLoad.length() == 5) {
+        m_rightWidth = fontMetrics().width(QString("↓0") + downLoad + QString(" "));
+    } else {
+        m_rightWidth = fontMetrics().width(QString("↓") + downLoad + QString(" "));
+    }
+    // 设置右侧字符串宽度预留15个像素
+    m_rightWidth += 15;
+
+
+    // 设置当前窗体固定大小
+    setFixedSize(m_leftWidth + m_rightWidth, fontMetrics().boundingRect(m_textList.at(0)).height() * 2);
+    update();
+}
+
+
+void SystemMonitorTipsWidget::paintEvent(QPaintEvent *event)
+{
+    QFrame::paintEvent(event);
+
+    int rectHeight = rect().height();
+    QPainter painter(this);
+    painter.setPen(QPen(palette().brightText(), 1));
+
+    QTextOption optionLeft;
+    optionLeft.setAlignment(Qt::AlignLeft | Qt::AlignVCenter);
+    QTextOption optionRight;
+    optionRight.setAlignment(Qt::AlignRight | Qt::AlignVCenter);
+    QTextOption optionMid;
+    optionMid.setAlignment(Qt::AlignCenter);
+
+    // 获取strList字符串，并存入cpu,mem,upLoad,download中
+    QString cpu;
+    QString mem;
+    QString upLoad;
+    QString downLoad;
+    for (int i = 0; i < m_textList.size(); ++i) {
+        if (i == 0)
+            cpu = m_textList.at(i);
+        else if (i == 1)
+            mem = m_textList.at(i);
+        else if (i == 2)
+            downLoad = m_textList.at(i);
+        else if (i == 3)
+            upLoad = m_textList.at(i);
+        else
+            qDebug() << "do not set the text";
+    }
+
+    // 当没有数据更新时，设置默认的悬浮框内容
     if (m_textList.size() == 0) {
         cpu = "0.0";
         mem = "0.0";
@@ -80,17 +124,29 @@ void SystemMonitorTipsWidget::paintEvent(QPaintEvent *event)
         downLoad = "0KB/s";
     }
 
-    int specialCharaWidth = fontMetrics().boundingRect(QString("↓")).width();
-    painter.drawText(QRectF(0.0, 0.0, rectWidth / 2.0, rectHeight / 2.0), QString("    ") + DApplication::translate("Plugin.cpu", "CPU") + QString(": ") + cpu + QString("%"), optionLeft);
+    int specialCharaWidth = fontMetrics().width(QString("↓"));
+    painter.drawText(QRectF(0.0, 0.0, m_leftWidth, rectHeight / 2.0), QString(" ") + DApplication::translate("Plugin.cpu", "CPU") + QString(": ") + cpu + QString("%"), optionLeft);
     painter.setPen(QPen(QColor("red"), 1));
-    painter.drawText(QRectF(rectWidth / 2.0, 0.0, specialCharaWidth, rectHeight / 2.0), QString("↓"), optionMid);
+    painter.drawText(QRectF(m_leftWidth, 0.0, specialCharaWidth, rectHeight / 2.0), QString("↓"), optionMid);
     painter.setPen(QPen(palette().brightText(), 1));
-    painter.drawText(QRectF(rectWidth / 2.0 + specialCharaWidth, 0, rectWidth / 2.0 - specialCharaWidth, rectHeight / 2.0), downLoad, optionLeft);
+    painter.drawText(QRectF(m_leftWidth + specialCharaWidth, 0, m_rightWidth, rectHeight / 2.0), downLoad, optionLeft);
 
-    specialCharaWidth = fontMetrics().boundingRect(QString("↑")).width();
-    painter.drawText(QRectF(0, rectHeight / 2.0, rectWidth / 2.0, rectHeight / 2.0), QString("    ") + DApplication::translate("Plugin.mem", "MEM") + QString(": ") + mem + QString("%"), optionLeft);
+    specialCharaWidth = fontMetrics().width(QString("↑"));
+    painter.drawText(QRectF(0, rectHeight / 2.0, m_leftWidth, rectHeight / 2.0), QString(" ") + DApplication::translate("Plugin.mem", "MEM") + QString(": ") + mem + QString("%"), optionLeft);
     painter.setPen(QPen(QColor("blue"), 1));
-    painter.drawText(QRectF(rectWidth /  2.0, rectHeight / 2.0, specialCharaWidth, rectHeight / 2.0), QString("↑"), optionMid);
+    painter.drawText(QRectF(m_leftWidth, rectHeight / 2.0, specialCharaWidth, rectHeight / 2.0), QString("↑"), optionMid);
     painter.setPen(QPen(palette().brightText(), 1));
-    painter.drawText(QRectF(rectWidth  / 2.0 + specialCharaWidth, rectHeight / 2.0, rectWidth / 2.0 - specialCharaWidth, rectHeight / 2.0), upLoad, optionLeft);
+    painter.drawText(QRectF(m_leftWidth + specialCharaWidth, rectHeight / 2.0, m_rightWidth, rectHeight / 2.0), upLoad, optionLeft);
+}
+
+bool SystemMonitorTipsWidget::event(QEvent *event)
+{
+    // 字体切换事件捕捉，当前获取到字体切换信号时，更新当前的悬浮框界面
+    if (event->type() == QEvent::FontChange) {
+        if (m_textList.size() > 0)
+            setSystemMonitorTipsText(m_textList);
+        else
+            setSystemMonitorTipsText(QStringList() << "0.0" << "0.0" << "0KB/s" << "0KB/s");
+    }
+    return QFrame::event(event);
 }
--- deepin-system-monitor-plugin/gui/systemmonitortipswidget.h
+++ deepin-system-monitor-plugin/gui/systemmonitortipswidget.h
@@ -40,8 +40,18 @@ protected:
     //!
     void paintEvent(QPaintEvent *event) override;
 
+    //!
+    //! \brief event 捕捉字体改变事件
+    //! \param event 捕获的事件对象
+    //! \return 成功返回true,失败返回false
+    //!
+    bool event(QEvent *event) override;
+
 private:
-    QStringList m_textList;
+    QStringList m_textList; // 获取到的悬浮框标签文字内容列表
+
+    int m_leftWidth = 0;    // 设置字体左侧的宽度
+    int m_rightWidth = 0;   // 设置字体右侧的宽度
 };
 
 #endif // SYSTEMMONITORTIPSWIDGET_H
