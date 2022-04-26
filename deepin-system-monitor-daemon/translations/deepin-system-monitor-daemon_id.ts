6e17241ed65f3210cbbb55322e5a0a1a77b2bd87
RiskVirusPro <95259731+RiskVirusPro@users.noreply.github.com>
2022-04-26 15:58:33 +0800

    feat:增加翻译文件和Loading文字 (#106)
    * feat: 增加loading框文字，更新翻译文件
    
    增加loading框文字，更新翻译文件
    
    Log: 增加loading框文字，更新翻译文件
    
    * feat: loading框的文字和翻译添加
    
    loading框的文字和翻译添加
    
    Log: loading框的文字和翻译添加

---
+++ deepin-system-monitor-daemon/translations/deepin-system-monitor-daemon_id.ts
@@ -0,0 +1,26 @@
+<?xml version="1.0" ?><!DOCTYPE TS><TS language="id" version="2.1">
+<context>
+    <name>SystemMonitorService</name>
+    <message>
+        <location filename="../systemmonitorservice.cpp" line="258"/>
+        <location filename="../systemmonitorservice.cpp" line="275"/>
+        <source>Warning</source>
+        <translation>Peringatan</translation>
+    </message>
+    <message>
+        <location filename="../systemmonitorservice.cpp" line="259"/>
+        <source>Your CPU usage is higher than %1%!</source>
+        <translation>Penggunaan CPU Anda lebih tinggi dari %1%!</translation>
+    </message>
+    <message>
+        <location filename="../systemmonitorservice.cpp" line="276"/>
+        <source>Your memory usage is higher than %1%!</source>
+        <translation>Penggunaan memori Anda lebih tinggi dari %1%!</translation>
+    </message>
+    <message>
+        <location filename="../systemmonitorservice.cpp" line="291"/>
+        <source>View</source>
+        <translation>Lihat</translation>
+    </message>
+</context>
+</TS>
