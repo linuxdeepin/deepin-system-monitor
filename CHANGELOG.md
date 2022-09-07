45ac2b58f91a9459a02675347d3d551dab22ad33
luominjiang <luominjiang@uniontech.com>
2022-09-07 16:48:46 +0800

    fix: 开源合规整改
    开源合规整改
    
    Log: 开源合规整改

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,9 @@
+<!--
+SPDX-FileCopyrightText: 2022 UnionTech Software Technology Co., Ltd.
+
+SPDX-License-Identifier: GPL-3.0-or-later
+-->
+
 <a name=""></a>
 ##  1.5.0 (2019-04-09)
 
--- tests/elfio.hpp
+++ tests/elfio.hpp
@@ -1,3 +1,6 @@
+// SPDX-FileCopyrightText: 2022 UnionTech Software Technology Co., Ltd.
+//
+// SPDX-License-Identifier: GPL-3.0-or-later
 
 /*** Start of inlined file: elfio_dump.hpp ***/
 #ifndef ELFIO_DUMP_HPP
