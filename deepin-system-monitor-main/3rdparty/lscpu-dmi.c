573c9401b0babc22c4541d1cfa88f93ffc215867
luominjiang <luominjiang@uniontech.com>
2022-10-18 14:41:12 +0800

    fix: 开源合规整改
    开源合规整改
    
    Log: 开源合规整改

--- deepin-system-monitor-main/3rdparty/lscpu-dmi.c
+++ deepin-system-monitor-main/3rdparty/lscpu-dmi.c
@@ -1,4 +1,4 @@
-// SPDX-License-Identifier: GPL-2.0-or-later
+// SPDX-License-Identifier: GPL-2.0
 /*
  * Copyright (C) 2020 FUJITSU LIMITED.  All rights reserved.
  */
--- deepin-system-monitor-main/dbus/dbusforsystemomonitorpluginservce.cpp
+++ deepin-system-monitor-main/dbus/dbusforsystemomonitorpluginservce.cpp
@@ -1,7 +1,3 @@
-// SPDX-FileCopyrightText: 2022 UnionTech Software Technology Co., Ltd.
-//
-// SPDX-License-Identifier: GPL-3.0-or-later
-
 #include "dbusforsystemomonitorpluginservce.h"
 #include "detailwidgetmanager.h"
 #include "application.h"
