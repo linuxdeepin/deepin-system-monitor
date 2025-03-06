a90602ecea4ae5f21375e93d86d20171118b9b00
Gong Heng <gongheng@uniontech.com>
2025-03-06 20:25:56 +0800

    fix: [system-monitor] remove dde-dock plugin
    remove dde-dock plugin

--- deepin-system-monitor-plugin-popup/main.cpp
+++ deepin-system-monitor-plugin-popup/main.cpp
@@ -4,7 +4,7 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
 
 #include "gui/mainwindow.h"
-#include "dde-dock/constants.h"
+//#include "dde-dock/constants.h"
 #include "application.h"
 
 //#include "clipboard_adaptor.h"
