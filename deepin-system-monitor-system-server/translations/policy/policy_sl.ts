83fdbd3242567c42eda7177696b566f7c9371b92
pengfeixx <63588879+pengfeixx@users.noreply.github.com>
2025-06-23 17:01:19 +0800

    fix: Fix translation errors (#436)
    Fix translation errors
    
    Log: Fix translation errors

--- deepin-system-monitor-system-server/translations/policy/policy_sl.ts
+++ deepin-system-monitor-system-server/translations/policy/policy_sl.ts
@@ -1,4 +1,15 @@
-<?xml version="1.0" encoding="utf-8"?>
-<!DOCTYPE TS>
-<TS version="2.1" language="sl">
+<?xml version="1.0" ?><!DOCTYPE TS><TS version="2.1" language="sl">
+	<context>
+		<name>policy</name>
+		<message>
+			<location filename="org.deepin.systemmonitor.systemserver.set!message" line="0"/>
+			<source>Authentication is required to set service startup type</source>
+			<translation>Zaustavljanje zacetnega vrata storitve je zahtevano prijavljenost</translation>
+		</message>
+		<message>
+			<location filename="org.deepin.systemmonitor.systemserver.set!description" line="0"/>
+			<source>Set service startup type</source>
+			<translation>Nastavi vrsto zacetnega vrata storitve</translation>
+		</message>
+	</context>
 </TS>
