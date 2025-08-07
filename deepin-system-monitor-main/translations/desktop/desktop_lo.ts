3e7c17e4d850ccf20404cf1e1866c8d10db32c62
dengzhongyuan <dengzhongyuan@uniontech.com>
2025-08-07 15:40:54 +0800

    chore: Add new Lao translation file
    Log: Improve language support by adding Lao translation

---
+++ deepin-system-monitor-main/translations/desktop/desktop_lo.ts
@@ -0,0 +1,20 @@
+<?xml version="1.0" ?><!DOCTYPE TS><TS version="2.1" language="lo">
+<context>
+    <name>desktop</name>
+    <message>
+        <location filename="Desktop Entry]Name" line="0"/>
+        <source>deepin System Monitor</source>
+        <translation>ບັນທຶກລະບົບ deepin</translation>
+    </message>
+    <message>
+        <location filename="Desktop Entry]Comment" line="0"/>
+        <source>Monitor system process status</source>
+        <translation>ຕິດຕາມສະພາບການຂະບວນການລະບົບ</translation>
+    </message>
+    <message>
+        <location filename="Desktop Entry]GenericName" line="0"/>
+        <source>System Monitor</source>
+        <translation>ເຄື່ອງມືຕິດຕາມລະບົບ</translation>
+    </message>
+</context>
+</TS>
---
+++ deepin-system-monitor-main/translations/policy/policy_lo.ts
@@ -0,0 +1,35 @@
+<?xml version="1.0" ?><!DOCTYPE TS><TS version="2.1" language="lo">
+<context>
+	<name>policy</name>
+	<message>
+		<location filename="com.deepin.pkexec.deepin-system-monitor.renice!message" line="0"/>
+		<source>Authentication is required to change process priority</source>
+		<translation>ການຢືນຢັນຕ້ອງການເພື່ອປ່ອນຄວາມສຳຄັນຂອງຂະບວນການ</translation>
+	</message>
+	<message>
+		<location filename="com.deepin.pkexec.deepin-system-monitor.renice!description" line="0"/>
+		<source>Renice process</source>
+		<translation>ປ່ອນຄວາມສຳຄັນຂອງຂະບວນການ</translation>
+	</message>
+	<message>
+		<location filename="com.deepin.pkexec.deepin-system-monitor.kill!message" line="0"/>
+		<source>Authentication is required to control other users&apos; processes</source>
+		<translation>ການຢືນຢັນຕ້ອງການເພື່ອຄຸ້ມຄອງຂະບວນການຂອງຜູ້ໃຊ້ອື່ນ</translation>
+	</message>
+	<message>
+		<location filename="com.deepin.pkexec.deepin-system-monitor.kill!description" line="0"/>
+		<source>Kill process</source>
+		<translation>ຍິງຂະບວນການ</translation>
+	</message>
+	<message>
+		<location filename="com.deepin.pkexec.deepin-system-monitor.systemctl!message" line="0"/>
+		<source>Authentication is required to set service startup type</source>
+		<translation>ການຢືນຢັນຕ້ອງການເພື່ອຕັ້ງປະເພດການເລີ່ມຕົ້ນຂອງບໍລິການ</translation>
+	</message>
+	<message>
+		<location filename="com.deepin.pkexec.deepin-system-monitor.systemctl!description" line="0"/>
+		<source>Set service startup type</source>
+		<translation>ຕັ້ງປະເພດການເລີ່ມຕົ້ນຂອງບໍລິການ</translation>
+	</message>
+</context>
+</TS>
