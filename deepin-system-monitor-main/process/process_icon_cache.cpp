6899e426001149a441b704213bb3f56d7ebf1b91
re2zero <yangwu@uniontech.com>
2025-09-05 08:41:37 +0800

    fix: Fix coredump issue
    - Added signal handling for graceful application exit using SIGTERM.
    - Updated main application logic to ensure proper cleanup and resource management.
    - Enhanced destructor implementations in NetifMonitorThread and SystemMonitorThread for safer thread termination and resource deallocation.
    - Ensured DBus service is unregistered before object destruction in MainWindow.
    
    Log: Fix coredump issue.

--- deepin-system-monitor-main/process/process_icon_cache.cpp
+++ deepin-system-monitor-main/process/process_icon_cache.cpp
@@ -22,17 +22,10 @@ ProcessIconCache::ProcessIconCache(QObject *parent)
     : QObject(parent)
 {
     qCDebug(app) << "ProcessIconCache object created";
-    // 由于之前获取dapplication::themetypechanged改变信号在有些平台获取不到，现通过获取DPlatformtheme方式获取
-    static QPointer<DPlatformTheme> theme;
-
-    if (!theme) {
-        qCDebug(app) << "DPlatformTheme not set, connecting to iconThemeNameChanged signal";
-        theme = DGuiApplicationHelper::instance()->applicationTheme();
-        connect(theme, &DPlatformTheme::iconThemeNameChanged, this, [ = ]() {
-            qCDebug(app) << "Icon theme changed, clearing pixmap cache";
-            iconPixmapCache.clear();
-        });
-    }
+    connect(DGuiApplicationHelper::instance(), &DGuiApplicationHelper::themeTypeChanged, this, [=]() {
+        qCDebug(app) << "Icon theme changed, clearing pixmap cache";
+        iconPixmapCache.clear();
+    });
 }
 
 } // namespace process
--- deepin-system-monitor-plugin-popup/model/process_table_model.cpp
+++ deepin-system-monitor-plugin-popup/model/process_table_model.cpp
@@ -38,15 +38,10 @@ ProcessTableModel::ProcessTableModel(QObject *parent)
     connect(ProcessDB::instance(), &ProcessDB::processKilled, this,
             &ProcessTableModel::removeProcess);
 
-    // 由于之前获取dapplication::themetypechanged改变信号在有些平台获取不到，现通过获取DPlatformtheme方式获取
-    static QPointer<DPlatformTheme> theme;
-
-    if (!theme) {
-        theme = DGuiApplicationHelper::instance()->applicationTheme();
-        connect(theme, &DPlatformTheme::iconThemeNameChanged, this, [=]() {
-            updateProcessList();
-        });
-    }
+    connect(DGuiApplicationHelper::instance(), &DGuiApplicationHelper::themeTypeChanged, this, [=]() {
+        qCDebug(app) << "theme changed, updating process list";
+        updateProcessList();
+    });
 }
 
 char ProcessTableModel::getProcessState(pid_t pid) const
